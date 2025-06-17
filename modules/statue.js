74bfd38987f37fb48646fd5cadb24dd3f13617e5
Alazar Andualem <alazarand@gmail.com>
2025-06-17 20:00:57 +0300

    adding statue

--- modules/statue.js
+++ modules/statue.js
@@ -2,33 +2,34 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 
+export let loadedStatue = null; // Export the statue object
+
 export const loadStatueModel = (scene) => {
   const loader = new GLTFLoader();
 
   loader.load(
-    "../public/models/statue/scene.gltf",
+    "../public/models/statue/girl_with_doves.glb",
     (gltf) => {
       const statue = gltf.scene;
+      loadedStatue = statue; // Assign to the exported variable
 
       // console.log("STATUE", gltf);
 
       // Position the statue at the center of the floor
-      statue.position.set(0, -3.2, 0);
+      statue.position.set(0, -2.9, 0);
 
       // Scale if necessary
-      statue.scale.set(0.06, 0.06, 0.06);
+      statue.scale.set(5.0, 5.0, 5.0);
 
       // Iterate through all the meshes in the statue and update their materials
       statue.traverse((child) => {
         if (child.isMesh) {
-          map: child.material.map,
-            // Modify child.material here to improve appearance
-            (child.material.metalness = 0.0),
-            (child.material.roughness = 0.2),
-            // Cast shadow
-            (child.castShadow = true);
-
-          // console.log("Statue Material:", child.material);
+          if (child.material) {
+            child.material.metalness = 0.0;
+            child.material.roughness = 0.2;
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
         }
       });
 
