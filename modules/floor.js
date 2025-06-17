37e07aed196ac0bae555840f4e5cf346117ce7f6
Alazar Andualem <alazarand@gmail.com>
2025-06-17 18:54:02 +0300

    fixed the floor

--- modules/floor.js
+++ modules/floor.js
@@ -1,41 +1,15 @@
 import * as THREE from "three";
-import floortTexture from "../public/WoodFloor040_4K-JPG/WoodFloor040_4K_Color.jpg";
 
 export const setupFloor = (scene) => {
   const textureLoader = new THREE.TextureLoader();
 
-  // Load the textures
-  const colorTexture = textureLoader.load(
-    "WoodFloor040_4K-JPG/WoodFloor040_4K_Color.jpg"
-  );
-  const displacementTexture = textureLoader.load(
-    "WoodFloor040_4K-JPG/WoodFloor040_4K_Displacement.jpg"
-  );
-  const normalTexture = textureLoader.load(
-    "WoodFloor040_4K-JPG/WoodFloor040_4K_NormalGL.jpg"
-  );
-  const roughnessTexture = textureLoader.load(
-    "WoodFloor040_4K-JPG/WoodFloor040_4K_Roughness.jpg"
-  );
-  const aoTexture = textureLoader.load(
-    "WoodFloor040_4K-JPG/WoodFloor040_4K_AmbientOcclusion.jpg"
-  );
-
-  // Set texture parameters
+  // Load the floor color texture from an existing image
+  const colorTexture = textureLoader.load("img/Floor.jpg");
   colorTexture.wrapS = colorTexture.wrapT = THREE.RepeatWrapping;
-  displacementTexture.wrapS = displacementTexture.wrapT = THREE.RepeatWrapping;
-  normalTexture.wrapS = normalTexture.wrapT = THREE.RepeatWrapping;
-  roughnessTexture.wrapS = roughnessTexture.wrapT = THREE.RepeatWrapping;
-  aoTexture.wrapS = aoTexture.wrapT = THREE.RepeatWrapping;
 
   const planeGeometry = new THREE.PlaneGeometry(45, 45);
   const planeMaterial = new THREE.MeshStandardMaterial({
     map: colorTexture,
-    displacementMap: displacementTexture,
-    normalMap: normalTexture,
-    roughnessMap: roughnessTexture,
-    aoMap: aoTexture,
-    displacementScale: 0.1,
     side: THREE.DoubleSide,
   });
 
